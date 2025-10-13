f1a0d7e83230f20e6a5fbe5659c3d14f8882aec2
kaka1-gba <196437868@qq.com>
2025-10-13 11:37:34 +0800

    Add files via upload

---
+++ text02.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*
+int main()
+{
+	printf("%d\n", 100);
+	return 0;
+}
+*/
+/*
+int main()
+{
+	printf("%zu\n", sizeof(char));
+	printf("%zu\n", sizeof(short));
+	printf("%zu\n", sizeof(int));
+	printf("%zu\n", sizeof(long));
+	printf("%zu\n", sizeof(long long));
+	printf("%zu\n", sizeof(float));
+	printf("%zu\n", sizeof(double));
+	return 0;
+}
+*/
+/*
+int main()
+{
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	return 0;
+}
+
+*/
