6f9602166e6425238c46c1f75d98ed34a558f207
kaka1-gba <196437868@qq.com>
2025-10-16 11:54:54 +0800

    commit

---
+++ text1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+	printf("No");
+	return 0;
+
+}
---
+++ text_10_14_1/text_10_14/text_001.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	//printf("hehe");
+	int a = 0;
+	scanf("%d", &a);
+	printf(" a=% d\n", a);
+
+	return 0;
+}
