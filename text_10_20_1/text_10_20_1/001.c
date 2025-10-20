d9f2ebc6e44337768401f7b6ea7666673256fe61
kaka1-gba <196437868@qq.com>
2025-10-20 10:05:13 +0800

    1

--- text_10_20_1/text_10_20_1/001.c
+++ text_10_20_1/text_10_20_1/001.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
 #include <stdbool.h>
 int main()
 {
@@ -7,3 +8,8 @@ int main()
 		printf("i like c\n");
 	return 0;
 }
+//#include <stdio.h>
+//int main() {
+//	sizeof(char);
+//	return 0;
+//}
